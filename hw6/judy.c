f8f03b4d6bd755ffeb0697c999029dc11e2ae825
Devin Schwab <digidevin@gmail.com>
2014-04-04 23:25:44 -0400

    Now have an example of client and server. Ran multiple instances of the clients at the same time and they didn't seem to conflict

--- hw6/judy.c
+++ hw6/judy.c
@@ -5,6 +5,44 @@
 int main(int argc, char** argv)
 {
 	printf("Starting judy\n");
+
+	char* server;
+	int value;
+	if(argc != 3)
+	{
+		fprintf(stderr, "usage: %s <host> <int>\n", argv[0]);
+		exit(1);
+	}
+
+	server = argv[1];
+	value = atoi(argv[2]);
+
+	// create client handle
+	CLIENT* client = clnt_create(server, COOKIE_PROGRAM, COOKIE_V1, "udp");
+	if(client == (CLIENT *)NULL)
+	{
+		clnt_pcreateerror(server);
+		exit(1);
+	}
+	int* result = increment_1(&value, client);
+	if(result == (int*)NULL)
+	{
+		clnt_perror(client, server);
+		exit(1);
+	}
+
+	printf("Increment result: %d\n", *result);
+
+	result = decrement_1(&value, client);
+	if(result == (int*)NULL)
+	{
+		clnt_perror(client, server);
+		exit(1);
+	}
+
+	printf("Decrement result: %d\n", *result);
+
+	clnt_destroy(client);
 	
 	return 0;
 }
--- hw6/mother.c
+++ hw6/mother.c
@@ -4,30 +4,31 @@
 
 int* increment_1(int* value, CLIENT* client)
 {
-	static int result = 10;
-
+	static int result;
 	printf("Calling increment with %d\n", *value);
 
+	result = *value + 1;
 	return &result;
 }
 
 int* increment_1_svc(int* value, struct svc_req* req)
 {
-	CLIENT* client;
+	CLIENT* client = NULL;
 	return (increment_1(value, client));
 }
 
 int* decrement_1(int* value, CLIENT* client)
 {
-	static int result = 9;
+	static int result;
 
 	printf("Calling decrement with %d\n", *value);
 
+	result = *value - 1;
 	return &result;
 }
 
 int* decrement_1_svc(int* value, struct svc_req* req)
 {
-	CLIENT* client;
+	CLIENT* client = NULL;
 	return (decrement_1(value, client));
 }
